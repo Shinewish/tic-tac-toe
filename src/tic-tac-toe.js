b3611eb3212fde6f51e7ad07a685c567bba6b744
AlexTheSunbeam <aliaksandr.tachko@gmail.com>
2017-01-30 22:49:34 +0300

    It's alive!

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,10 +2,10 @@ class TicTacToe {
     constructor() {
         this.curPlaySymb = "x";
         this.sum = 0;
-        this.isDrw = false;
+        this.field = [[null, null, null], [null, null, null], [null, null, null]];
         this.noTurns =false;
         this.winner = null;
-        this.field = [[,,],[,,],[,,]];
+
     }
 
     getCurrentPlayerSymbol() { 
@@ -15,16 +15,40 @@ class TicTacToe {
     nextTurn(rowIndex, columnIndex) {
         if (this.field[rowIndex][columnIndex] === null) {
             this.field[rowIndex][columnIndex] = this.curPlaySymb;
-            this.curPlaySymb = this.curPlaySymb === "x" ? "o" :"x";
+            this.curPlaySymb = this.curPlaySymb === "x" ? "o" : "x";
             this.sum++;
             }
     }
 
     isFinished() {
-        return this.isFin;
+        return (!this.getWinner() && !this.isDraw()) ? false : true;
     }
 
     getWinner() {
+        if (this.field[0][0] + this.field[0][1] + this.field[0][2] === "xxx"||
+            this.field[1][0] + this.field[1][1] + this.field[1][2] === "xxx"||
+            this.field[2][0] + this.field[2][1] + this.field[2][2] === "xxx"||
+
+            this.field[0][0] + this.field[1][0] + this.field[2][0] === "xxx"||
+            this.field[0][1] + this.field[1][1] + this.field[2][1] === "xxx"||
+            this.field[0][2] + this.field[1][2] + this.field[2][2] === "xxx"||
+
+            this.field[0][0] + this.field[1][1] + this.field[2][2] === "xxx"||
+            this.field[0][2] + this.field[1][1] + this.field[2][0] === "xxx")
+            {this.winner = "x"} ;
+
+        if (this.field[0][0] + this.field[0][1] + this.field[0][2] === "ooo"||
+            this.field[1][0] + this.field[1][1] + this.field[1][2] === "ooo"||
+            this.field[2][0] + this.field[2][1] + this.field[2][2] === "ooo"||
+
+            this.field[0][0] + this.field[1][0] + this.field[2][0] === "ooo"||
+            this.field[0][1] + this.field[1][1] + this.field[2][1] === "ooo"||
+            this.field[0][2] + this.field[1][2] + this.field[2][2] === "ooo"||
+
+            this.field[0][0] + this.field[1][1] + this.field[2][2] === "ooo"||
+            this.field[0][2] + this.field[1][1] + this.field[2][0] === "ooo")
+            {this.winner = "o"}
+
         return this.winner;
     }
 
@@ -33,7 +57,7 @@ class TicTacToe {
     }
 
     isDraw() {
-        return !this.getWinner() && this.sum === 9 ? true : false; 
+        return (this.getWinner() === null && this.sum === 9) ? true : false; 
     }
 
     getFieldValue(rowIndex, colIndex) {
