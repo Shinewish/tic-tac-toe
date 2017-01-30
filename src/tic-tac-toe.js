c7f78faf4a2b3a292019c09daf1d6c390eef1a23
Aliaksandr Tachko <shinewish@mail.ru>
2017-01-30 18:37:41 +0200

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,22 +1,29 @@
 class TicTacToe {
     constructor() {
-
+        this.curPlaySymb = x;
+        this.isFin = false;
+        this.isDrw = false;
+        this.noTurns =false;
+        this.winner = null;
+        this.field = [[,,],[,,],[,,]];
     }
 
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() { 
+        return this.curPlaySymb;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.field[rowIndex][columnIndex] === null) {
+            this.field[rowIndex][columnIndex] = this.curPlaySymb;    
+            }
     }
 
     isFinished() {
-
+        return this.isFin;
     }
 
     getWinner() {
-
+        return this.winner;
     }
 
     noMoreTurns() {
@@ -24,11 +31,11 @@ class TicTacToe {
     }
 
     isDraw() {
-
+        return this.isDrw;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
